a1a1b02764088538ab5890360b455ffecb95f683
GAJIbl4 <54286964+GAJIbl4@users.noreply.github.com>
2021-08-15 13:01:52 +0300

    Лабораторная работа №3
    Разработать схему алгоритма, составить Pascal‐программу вычисления таблицы значений функции. 
    Если аргумент Х изменяется от начального значения Xn до Xk с шагом Dx, параметр А принимает M значений, а параметр B принимает значение, равное первому положительному корню нелинейного уравнения.

--- Pascal/Program.pas
+++ Pascal/Program.pas
@@ -1,62 +1,153 @@
-﻿type
-  Ar = array [1 .. 50] of real;
+﻿uses CRT;
 
-procedure DataIn(Xn, Dx: real; K: integer; var X: Ar);
-var
-  I: integer;
-begin
-  X[1] := Xn;
-  for I := 2 to K do
+const
+  Nmax = 40;
+
+Type
+  Tmy = array [1 .. Nmax, 1 .. Nmax] of real;
+  Tma = array [1 .. Nmax] of real;
+  Ter = array [1 .. Nmax, 1 .. Nmax] of boolean;
+
+procedure Equat(C, D, Eps: real; Km: integer; var B, Bt: real;
+  var Err: integer);
+
+  Function F(X: real): real;
   begin
-    X[I] := X[I - 1] + Dx;
+    if X=0 then X:=2E-38;
+    F := 2*ln(X)-1/power(X,2);
   end;
-end;
 
-procedure Series(X1, Eps, R, A: real; Km: integer; var S1: real);
 var
   I: integer;
+
 begin
-  S1 := 0;
-  R := 1;
+  Err := 1;
   I := 1;
-  repeat
+  while I <= Km do
   begin
-    S1 := S1 + R;
-    R := R * ((X1 + A) / I);
-    if (abs(R) < Eps) then
-      I := Km
+    B := (C + D) / 2;
+    if abs(F(B)) > Eps then
+    begin
+      if F(C) * F(B) > 0 then
+        C := B
+      else
+        D := B;
+    end
     else
-      I := I + 1;
+    begin
+      I := Km;
+      Err := 0;
+    end;
+    I := I + 1;
   end;
-  until I >= Km;
+  Bt := abs(F(B));
 end;
 
-procedure DataOut(X, S: Ar; K: integer; A: real);
+procedure Tab(B: real; M, N: integer; Ma, Mx: Tma; var My: Tmy; var Er: Ter);
+
+var
+  I, J: integer;
+  A, X: real;
+begin
+  for I := 1 to M do
+    for j := 1 to N do
+        if (Ma[I] = 0) or ((power(Ma[I],2)+power(B,2))<=0) then
+          Er[j, I] := true
+        else
+          Er[j, I] := false;
+
+  For I := 1 to M do
+  begin
+    A := Ma[I];
+    For j := 1 to N do
+    begin
+      X := Mx[j];
+      if (Er[j, I] = false) then
+          My[j, I] := B/(A*sqrt(power(A,2)+power(B,2)))*arctan(A*X/(sqrt(power(A,2)+power(B,2))));
+    end;
+  end;
+end;
+
+procedure DataIn(Xn, Dx, Xk: real; M: integer; var Ma, Mx: Tma; var N:integer);
+
 var
   I: integer;
 begin
-  writeln('A=', A);
-  for I := 1 to K do
+  for I := 1 to M do
   begin
-    writeln('X[', I, ']=', X[I], '   Sum=', S[I]);
+    write('A[',I,']=');
+    readln(Ma[I]);
   end;
+
+  Mx[1] := Xn;
+  N:=floor((Xk - Xn) / Dx) + 1;
+  for I:=2 to N do 
+  Mx[I] := Mx[I - 1] + Dx;
+   
 end;
 
+procedure DataOut(M, N: integer; B, Bt: real; Ma, Mx: Tma; My: Tmy;
+
+  Er: Ter);
 var
-  X, S: Ar;
-  Eps, R, A, Xn, Dx: real;
-  Km, K, I: integer;
-
+  I, j: integer;
 begin
-  Eps := 0.0001;    //Погрешность
-  Km := 50;         //Количество итераций
-  A := 1;           //Параметр А
-  Xn := 0;          //Начальное значение X
-  Dx := 1;          //Шаг X
-  K := 5;           //Количество X
+    write('Корень уравнения: ');
+    writeln(B:10:9);
+    write('Погрешность по невязке: ');
+    writeln(Bt:10:9);
 
-  DataIn(Xn, Dx, K, X);
-  for I := 1 to K do
-    Series(X[I], Eps, R, A, Km, S[I]);
-  DataOut(X, S, K, A);
+    write('         ');
+    for J := 1 to N do
+    begin
+      write('X[',J,']=',Mx[J]:2:1,' ');
+    end;
+    writeln;
+    for I := 1 to M do
+    begin
+    write('A[',I,']=',Ma[I]:2:1,' ');
+      for J := 1 to N do
+        begin
+        write(My[J,I]:7:3,'  ');
+        end;
+    writeln;
+    end;
+end;
+
+
+var
+  Ma, Mx: Tma;
+  My: Tmy;
+  Er: Ter;
+  Xn, Dx, Xk, B, Bt, Eps, C, D: real;
+  Km, M, N, Err: integer;
+begin
+  write('Xn=');
+  readln(Xn);
+  write('Dx=');
+  readln(Dx);
+  write('Xk=');
+  readln(Xk);
+  write('M=');
+  readln(M);
+  DataIn(Xn, Dx, Xk, M, Ma, Mx, N);
+  write('C=');
+  readln(C);
+  write('D=');
+  readln(D);
+  write('Eps=');
+  readln(Eps);
+  write('Km=');
+  readln(Km);
+  
+
+  Equat(C, D, Eps, Km, B, Bt, Err);
+  if Err = 0 then
+  begin
+    Tab(B, M, N, Ma, Mx, My, Er);
+    DataOut(M, N, B, Bt, Ma, Mx, My, Er);
+  end
+  else
+    writeln('Нужно больше итераций!!!');
+    readln;
 end.
